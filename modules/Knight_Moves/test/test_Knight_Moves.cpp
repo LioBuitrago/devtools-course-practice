7b9ec3c185449e9e50e143ad08af942a0b3c46f0
LioBuitrago <jcgb20@gmail.com>
2021-04-26 23:57:30 +0300

    In process. spaces

--- modules/Knight_Moves/test/test_Knight_Moves.cpp
+++ modules/Knight_Moves/test/test_Knight_Moves.cpp
@@ -8,7 +8,7 @@ TEST(Galindo_Knight_Moves, Minimum_without_obstacles_i) {
     KM sol;
     int x = 10;
     int y = 12;
-    int result = sol.valor_minimo_knight(x,y);
+    int result = sol.valor_minimo_knight(x, y);
     int expectation = 8;
     ASSERT_EQ(expectation, result);
 }
@@ -17,7 +17,7 @@ TEST(Galindo_Knight_Moves, Minimum_without_obstacles_ii) {
     KM sol;
     int x = 30;
     int y = 28;
-    int result = sol.valor_minimo_knight(x,y);
+    int result = sol.valor_minimo_knight(x, y);
     int expectation = 20;
     ASSERT_EQ(expectation, result);
 }
@@ -26,7 +26,7 @@ TEST(Galindo_Knight_Moves, Minimum_without_obstacles_iii) {
     KM sol;
     int x = 40;
     int y = 38;
-    int result = sol.valor_minimo_knight(x,y);
+    int result = sol.valor_minimo_knight(x, y);
     int expectation = 26;
     ASSERT_EQ(expectation, result);
 }
